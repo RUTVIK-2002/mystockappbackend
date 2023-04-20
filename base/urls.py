99a4f58fe0aa18f6c2e6b68e0652a50dab48b021
rutvik nelluri <rutviknelluri19@gmail.com>
2023-04-20 15:14:04 +0530

    .

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes),
-    #path('stocks/', views.getStocks),
-    #path('stocks/<str:pk>', views.getStock),
+    path('', views.Home, name='home'),
+    path('home/', views.Home, name='home'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,26 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from base.models import Room
-#from . serializers import RoomSerializer
-#from 
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def getRoutes(request):
-    p = 10
-    return p
+def loginPage(request):
+    #page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+    #context = {'page': page}
+    return render(request, 'base/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def Home(request):
+    return render(request, 'base/home.html')
