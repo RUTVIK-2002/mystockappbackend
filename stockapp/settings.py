096c97fef00fdae54c420c2ab82553c92a95c0fe
rutvik nelluri <rutviknelluri19@gmail.com>
2023-04-20 21:25:33 +0530

    .

--- stockapp/settings.py
+++ stockapp/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #CORS_ALLOW_ALL_ORIGINS = True
-CSRF_COOKIE_SECURE = False
+#CSRF_COOKIE_SECURE = False
 #SESSION_COOKIE_SECURE = False
 CORS_ALLOWED_ORIGINS = [
     "https://djangostockapp.azurewebsites.net",
